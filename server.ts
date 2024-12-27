321da1f542a8dc0b94aa60ae4b5a383a02669e4d
Juan Torres <jtorresramirez.19@gmail.com>
2024-12-26 23:00:51 -0300

    FEAT | End section 31 - Rollback deploy in netlify

--- server.ts
+++ server.ts
@@ -4,7 +4,6 @@ import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import AppServerModule from './src/main.server';
-import { render } from "@netlify/angular-runtime/src/common-engine";
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -14,10 +13,6 @@ export function app(): express.Express {
   const indexHtml = join(serverDistFolder, 'index.server.html');
 
   const commonEngine = new CommonEngine();
-
-  async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
-    return await render(commonEngine)
-  }
 
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
